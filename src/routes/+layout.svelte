1e3134eb4899310fc7df45db7111723c481da222
Domantas-S <d.saltenis1@gmail.com>
2023-10-29 18:39:07 +0000

    initial home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,40 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
+	import modeNight from '@iconify/icons-material-symbols/mode-night';
+	import { goto } from '$app/navigation';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+
 </script>
+
+
+<AppBar class="py-2 border-b-2 trail">
+	<svelte:fragment slot="lead">
+		<button type="button" on:click={() => {goto('/')}} class="btn btn-sm variant-filled">
+			Home
+		</button>
+	</svelte:fragment>
+
+	<svelte:fragment>
+		<button type="button" on:click={() => {goto('/analysis')}} class="btn btn-sm variant-filled">
+			Data Analysis
+		</button>
+	</svelte:fragment>
+	
+	<svelte:fragment slot="trail">
+		<button type="button" class="btn-icon variant filled">
+			<Icon icon={modeNight}/>
+		</button>
+	</svelte:fragment>
+	
+	
+</AppBar>
+<div class="py-6"></div>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	import Background from '$lib/layered-waves-haikei.svelte';
+</script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+<div class="container h-full mx-auto flex justify-center items-centre">
+	<div class="space-y-5 text-center">
+		<h1 class="h1">Analysing and Visualising ADEs with LLMs</h1>
+		<p class="text-sm">Utilising the PHEE dataset, retrieve data and visualise patterns using natural language textual inputs.</p>
+		<div class="py-5"></div>
+		<button type="button" class="btn variant-filled rounded py-2 px-20">
+			<p>Try it out</p>
+		</button>
+
 	</div>
+	
 </div>
+<!-- <Background />	 -->
