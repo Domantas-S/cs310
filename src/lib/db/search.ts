b554a5e1aa65f6976672ca882f0a88c03c726b8b
Domantas-S <d.saltenis1@gmail.com>
2024-01-21 22:06:48 +0000

    Use full dataset

--- src/lib/db/search.ts
+++ src/lib/db/search.ts
@@ -1,7 +1,7 @@
 import sql from "./db";
 
 export async function searchRecord(keyword : string) {
-    const result = await sql`select * from dev2 where position(lower(${keyword}) in lower(context)) > 0;`;
+    const result = await sql`select * from phee where position(lower(${keyword}) in lower(context)) > 0;`;
     return result;
 }
 
@@ -10,13 +10,13 @@ export async function searchAnnotation(key : string){
         const result = await sql`
             SELECT
                 *
-            FROM dev2;`
+            FROM phee;`
         return result;
     }
     const result = await sql`
         SELECT 
             *
-        FROM dev2,
+        FROM phee,
             jsonb_array_elements(annotations->0->'events') with ordinality arr(event_obj, event_num)
         WHERE arr.event_obj ? ${key};`
 
@@ -30,7 +30,7 @@ export async function searchAnnotationWithSubkey(key : string, subkey : string){
     const result = await sql`
         SELECT 
             *
-        FROM dev2,
+        FROM phee,
             jsonb_array_elements(annotations->0->'events') with ordinality arr(event_obj, event_num)
         WHERE arr.event_obj ? ${key} AND arr.event_obj->${key} ? ${subkey};`
     
@@ -44,7 +44,7 @@ export async function searchAnnotationWithMatching(key: string, subkey: string, 
         const result = await sql`
         SELECT 
             *
-        FROM dev2,
+        FROM phee,
             jsonb_array_elements(annotations->0->'events') with ordinality arr(event_obj, event_num)
         WHERE POSITION(LOWER(${target}) IN LOWER(context)) > 0;`
         return result;
@@ -53,7 +53,7 @@ export async function searchAnnotationWithMatching(key: string, subkey: string, 
         const result = await sql`
         SELECT 
             *
-        FROM dev2,
+        FROM phee,
             jsonb_array_elements(annotations->0->'events') with ordinality arr(event_obj, event_num)
         WHERE arr.event_obj ? ${key} AND POSITION(LOWER(${target}) IN LOWER(context)) > 0;`
         return result;
@@ -62,7 +62,7 @@ export async function searchAnnotationWithMatching(key: string, subkey: string, 
         const result = await sql`
         SELECT 
             *
-        FROM dev2,
+        FROM phee,
             jsonb_array_elements(annotations->0->'events') with ordinality arr(event_obj, event_num)
         WHERE arr.event_obj ? ${key} AND arr.event_obj->${key} ? ${subkey} AND POSITION(LOWER(${target}) IN LOWER(context)) > 0;`
         return result;
