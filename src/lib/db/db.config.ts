7e2a8ec65b2858dcb0ef6efee8b571960be70f90
Domantas Saltenis <d.saltenis1@gmail.com>
2024-03-19 18:37:47 +0000

    updated db config again

--- src/lib/db/db.config.ts
+++ src/lib/db/db.config.ts
@@ -1,11 +1,16 @@
 
 const prodBackendURL = "https://annotate.db.drugwatch.net"; 
-const URL = import.meta.env.DEV ? "localhost" : prodBackendURL;
 
-export const config = {
-    host: URL,
+export const config = import.meta.env.DEV ? {
+    host: "localhost",
     port: 5444,
     database: 'phee',
     user: 'postgres',
     password: 'postgres'
-  };
+  } : {
+    host: prodBackendURL,
+    port: 80,
+    database: 'phee',
+    user: 'postgres',
+    password: 'postgres'
+  }
